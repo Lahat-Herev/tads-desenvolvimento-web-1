c10aa58ac44a51316309ca92e1ffb74989f56cba
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-07-04 00:19:44 -0300

    Preview of exercise 2 and removed unused link of exercise 2

---
+++ exercicio-2-javascript.js
@@ -0,0 +1,29 @@
+let numeroUm = document.getElementById("numberOne");
+let numeroDois = document.getElementById("numberTwo");
+let botaoGerar = document.getElementById("gerarNumeros");
+let respostaUsuario = document.getElementById("userResult");
+let resultado = document.getElementById("message");
+
+botaoGerar.addEventListener("click", () => generateRandomNumber());
+
+respostaUsuario.addEventListener("change", () => 
+    verificaIgualdadeCalculo(somaNumeros(numeroUm.value, numeroDois.value), respostaUsuario.value)
+);
+
+function generateRandomNumber()
+{
+    numeroUm.value = Math.floor(Math.random() * 100);
+    numeroDois.value = Math.floor(Math.random() * 100);
+}
+
+function somaNumeros(first, second)
+{
+    return parseInt(first) + parseInt(second);
+}
+
+function verificaIgualdadeCalculo(first, second)
+{
+    let mensagem;
+    mensagem = first == second ? `A soma dos numeros gerados (${first}) é igual ao resultado calculado (${second}). ` : `A soma dos numeros gerados (${first}) é diferente do resultado calculado (${second}).  `;
+    resultado.innerText = mensagem;
+}
