778a13cb4ea15e22d4700d53aaf321ff3bbf9077
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-07-05 00:39:56 -0300

    Test for exercise 3

--- exercicio-3-javascript.js
+++ exercicio-3-javascript.js
@@ -1,13 +1,20 @@
-// let product = document.get
+const productName = document.getElementById("product-name").value;
+const productPrice = document.getElementById("product-price").value;
+const saveProduct = document.getElementById("saveProduct");
+const result = document.getElementById("result");
 
-// const produto = [
-//     {
-//         name, 
-//         price
-//     }
-// ];
+const produtos = {
+    item: [{
+        productName,
+        productPrice
+    }]
+};
 
-// function criaProduto({nome, preco})
-// {
+function criaProduto(dados) {
+    produtos.item.push({
+        productName : dados.productName,
+        productPrice : dados.productPrice
+    });
+}
 
-// }
+saveProduct.addEventListener("click", criaProduto({productName : productName, productPrice : productPrice}));
