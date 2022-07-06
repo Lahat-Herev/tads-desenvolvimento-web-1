1012e6011db2ee4b0f2adc728a0749f2d5d24e98
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-07-06 15:27:33 -0300

    Almost ended the style things of exercise 3

--- exercicio-3-javascript.js
+++ exercicio-3-javascript.js
@@ -16,14 +16,12 @@ function carregarProdutos()
         let precoProduto = element.precoProduto;
         let $product_container = `
         <section class="product">
-            <section>
-            <h2>Digite as informações do produto: </h2>
+            <h2>Digite as informações do produto </h2>
             <input class="nome" title="Nome do produto" type="text" value="${nomeProduto}">
             <input class="preco" title="Preço do produto" type="text" value="${precoProduto}">
             <section class="action">
                 <a href="#" class="salvar">Salvar</a>
                 <a href="#" class="remover">Remover</a>
-            </section>
             </section>
         </section>
         `;
