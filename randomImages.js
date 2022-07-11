2397ec6f777554f2ddf6ea6198f1ef10e0acdd38
Pedro Arthur Alves de Oliveira <pedroarthuralvesdeoliveira1@gmail.com>
2022-07-10 22:27:36 -0300

    First try of async/await

--- randomImages.js
+++ randomImages.js
@@ -2,11 +2,32 @@ const imagens = ["bolinha.jpg", "breedito.png", "perrito.jpg", "winer.jpg"];
 const switchImage = document.getElementById("changeImage");
 const img = document.getElementById("image-picker"); 
 
-function changeImage()
+function randomImage()
 {
-    let newImage = Math.floor(Math.random() * imagens.length);
-    src = `images/${imagens[newImage]}`
-    return img.src = `${src}`;
+    return Math.floor(Math.random() * imagens.length);
+}
+
+function result()
+{
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(randomImage());
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+// const result = new Promise((resolve, reject) => {
+//    try {
+//     resolve(randomImage())
+//    } catch (error) {reject(error)}
+// });
+
+async function changeImage()
+{
+    let newImage = await result();
+    return img.src = `images/${imagens[newImage]}`;
 }
 
 switchImage.addEventListener("click", changeImage);
